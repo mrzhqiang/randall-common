603296c5e9c6a86f77fcb1a6602506f741864300
mrzhqiang <287431404@qq.com>
2020-02-15 17:47:59 +0800

    更新：新增一个信号常量类

---
+++ src/main/java/randall/common/Signal.java
@@ -0,0 +1,11 @@
+package randall.common;
+
+public enum Signal {
+  ;
+
+  public static final String ACTIVE = "----%AA%----";
+  public static final String BEGIN = "----%BB%----";
+  public static final String EXIT = "----%EE%----";
+  public static final String FINISHED = "----%FF%----";
+  public static final String PING = "----%PP%----";
+}
